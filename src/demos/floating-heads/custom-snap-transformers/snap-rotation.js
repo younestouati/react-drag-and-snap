9946ef097d1a172fcbf64bb0b7a04cf750673a32
Younes Peters Touati <younes.touati@citrix.com>
2018-03-01 21:46:50 +0100

    Small simplification

--- src/demos/floating-heads/custom-snap-transformers/snap-rotation.js
+++ src/demos/floating-heads/custom-snap-transformers/snap-rotation.js
@@ -2,9 +2,7 @@ import {EDGES, getClosestEdge} from '../utils/edge-utils';
 import SnapTransformers from '../../../lib/defaults/default-snap-transformers';
 import {extend} from '../utils/extend';
 
-const snapRotation = (draggableDescriptor, {width, height}) => {
-	const targetSize = {width, height};
-
+const snapRotation = (draggableDescriptor, targetSize) => {
 	return extend(
 		SnapTransformers.noSnapping(draggableDescriptor),
 		{rotate: EDGES[getClosestEdge(draggableDescriptor.transform, targetSize).edge].rotation}
